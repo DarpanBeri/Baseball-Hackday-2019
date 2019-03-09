7405a8310d46f64498b583fc7260ddba88c0f3be
mordva42 <333marina2007@gmail.com>
2019-03-09 21:14:14 +0000

    added sudocode

--- Algo.py
+++ Algo.py
@@ -1,4 +1,7 @@
 "
     Algorithm that chooses where the user to go to get his food.
-
+    1) Based on food type find the shortestLine 
+    2) Based on food type fin the closestLine
+    3) if (closestLine-shortestLine)<3 go to closestLine else go to shortestLine
+    
 "
